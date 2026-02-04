37645ea706d45f637c3e2da30ec5c2336c27e919
luizabarros <thecoderana@gmail.com>
2026-02-04 19:29:46 -0300

    fix: search

--- src/lib/search.ts
+++ src/lib/search.ts
@@ -117,7 +117,7 @@ export class SearchAPI {
     const searchPromises = [
       this.searchSerpAPI({
         engine: 'google',
-        q: `${searchTerm} ficha técnica -site:carrosnaweb.com.br -site:byd.com`,
+        q: `${searchTerm} ficha técnica -site:carrosnaweb.com.br -site:byd.com -filetype:pdf`,
       }),
 
       this.searchSerpAPI({
